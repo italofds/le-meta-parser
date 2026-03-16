7df7815e34944df96c808f8d13fd4c1980c0bd2a
Ítalo Santos <italobragasantos@hotmail.com>
2026-03-16 19:13:03 -0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 const allGroups = document.querySelectorAll('div.t.o');
 
-let result = {};
+let result = [];
 let stack = [result];
 
 const results = Array.from(allGroups).forEach(el => {
@@ -22,38 +22,61 @@ const results = Array.from(allGroups).forEach(el => {
     }
 	
 	const valueEl = el.querySelector('div.m > div');
-	let groupText;
-    if (valueEl) {
-        const hasChild = valueEl.querySelector('div.t.o') !== null;
-        if (!hasChild) {
-            groupText = valueEl.textContent.trim()!='' ? valueEl.textContent.trim() : null;
-        } else {
-            groupText = null;
-        }
+	let groupText = null;
+    if (valueEl && valueEl.querySelector('div.t.o') === null && valueEl.textContent.trim()) {
+		const DIVIDER = "[LIST BREAK]"
+		const markedHTML = valueEl.innerHTML.replace(/<div class="p"><\/div>/g, DIVIDER);
+		valueEl.innerHTML = markedHTML;
+		const listValues = valueEl.textContent.split(DIVIDER).map(item => item.trim()).filter(item => item !== "");
+		
+		if(listValues.length == 1) {
+			groupText = listValues[0];
+		} else {
+			groupText = listValues;
+		}
     }
 	
-	if(groupKey) {	
-		const isObject = groupText === null;
-        const currentParent = stack[level];
-    
-        if (isObject) {
-			var newArray = [{}];
-            currentParent[groupKey] = newArray; 
-            stack[level + 1] = currentParent[groupKey];
-			
-        } else {			
-			if (Array.isArray(currentParent)) {
-				if (Object.keys(currentParent[0])[0] == groupKey) {
-					currentParent.push({});
-				}
-				var obj = currentParent[currentParent.length-1];
-				obj[groupKey] = groupText;
-				
-			} else {
-				currentParent[groupKey] = groupText;
-			}
-        }
+	if(groupKey) {
+		let childrenNodes = [];
+		let parentObj = stack[level];
+		stack[level+1] = childrenNodes;
+		parentObj.push({
+			key: groupKey,
+			value: groupText,
+			level: level,
+			children: childrenNodes
+		}); 		
 	}
 });
 
-console.log(result);
+function formatObj(obj,data) {
+	if(data.children.length) {
+		if(data.value) {
+			obj[data.key] = {value : data.value, attributes : formatList(data.children)};
+		} else {
+			obj[data.key] = formatList(data.children);
+		}
+	} else {
+		obj[data.key] = data.value;
+	}
+	return obj;
+}
+
+function formatList(list) {	
+	if(list.length == 1) {
+		return formatObj({}, list[0]);
+		
+	} else {
+		let array = [];		
+		list.forEach(el => {
+			if(!array.length || Object.keys(array[0])[0] === el.key) {
+				array.push({});
+			}
+			let obj = array[array.length-1];
+			obj = formatObj(obj, el);
+		});		
+		return array;
+	}
+}
+
+console.log(formatList(result)[0]);
